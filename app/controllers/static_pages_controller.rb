0a569f13b2f8b8ac15a71e62b7cec3ecbb8a0fc4
MAYUMI <cova076@gmail.com>
2019-10-16 15:19:36 +0000

    6章6.2.2

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,15 @@
 class StaticPagesController < ApplicationController
+  
   def top
   end
+  
+  def create
+    user =User.find_by(email: params[:session]{:email}.downcase)
+    if user && user.authenticate(params[:session][:password])
+ 
+    else
+     
+      render :new
+    end
+  end   
 end
